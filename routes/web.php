26f76dccfa3e57a1964b85fe02e98a5ff9a274e0
3A532092 <star1998410@gmail.com>
2018-11-17 22:01:00 +0800

    建立任務控制器  對應控制器的路由  認證所有的路由

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ Route::get('/', function () {
 });
 
 Route::auth();
+
+Route::get('/tasks', 'TaskController@index');
+
+Route::post('/task', 'TaskController@store');
+
+Route::delete('/task/{task}', 'TaskController@destroy');
