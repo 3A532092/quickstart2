5ad77badcf02dfa7d664120a2fcdea8fedf2819f
3A532092 <star1998410@gmail.com>
2018-11-17 22:40:36 +0800

    新增/home路徑  建立index.blade.php和errors.blade.php 定義TaskController的index方法

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/tasks', 'TaskController@index');
 Route::post('/task', 'TaskController@store');
 
 Route::delete('/task/{task}', 'TaskController@destroy');
+
+Route::get('/home',function(){
+    return view('home.home');
+});
