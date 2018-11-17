7f6cd698ae21c02ffe7a299bf21fb852becd4ce3
3A532092 <star1998410@gmail.com>
2018-11-17 21:48:58 +0800

    建立關聯

--- app/Task.php
+++ app/Task.php
@@ -4,8 +4,15 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Task extends Model
 {
     //
     protected $fillable = ['name'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
